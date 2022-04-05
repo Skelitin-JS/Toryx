43831420cb95794db0637013594e9f7a61227cd2
Tavin <2009ratsnake@gmail.com>
2022-04-04 21:00:33 -0500

    added login

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const discord = require("discord.js")
+const Discord = require("discord.js")
+
+
+const TOKEN = "OTQzNzIwNzA0NzA3MjY0NTMy.Yg3KZQ.eP_iWZbTbWi-bn2REE1xWMMrphQ"
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as${client.user.tag}`)
+})
+
+client.login(TOKEN)
